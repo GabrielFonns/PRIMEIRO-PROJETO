2b92608a54c6b6b58991a128575fae37c0550703
GabrielFonns <89812628+GabrielFonns@users.noreply.github.com>
2022-06-15 15:59:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const respostaElement = document.querySelector('#resposta')
+const inputPergunta = document.querySelector('#inputPergunta')
+const buttomPerguntar= document.querySelector("#buttomPerguntar")
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+
+function fazerPergunta() {
+
+  if (inputPergunta.value == "")  {
+    alert('digite uma pergunta, por favor mew.')
+    return
+  }       
+
+  buttomPerguntar.setAttribute("disabled", true) 
+
+  const pergunta = "<div>" + inputPergunta.value  +
+  "</div>"
+
+const TotalResposta = respostas.length
+const numeroAleatorio = Math.floor(Math.random() * TotalResposta)
+
+  respostaElement.innerHTML = pergunta + respostas[numeroAleatorio]
+
+respostaElement.style.opacity= 1;
+  setTimeout(function( ) {
+    respostaElement.style.opacity= 0;
+    buttomPerguntar.removeAttribute("disabled")
+
+  },3000)
+}
+  
+ 
---
+++ index.md
@@ -0,0 +1,15 @@
+Hyper text marckup language 
+
+Hiper texto??
+- Marcação 
+-atributos
+- tags
+- linguagem
+ -maneira de escrever
+  
+  oq é hypertext?
+  - são marcações com certos atributos que possuem tags para determinadas funções
+  oq é marckup
+  oq é tag?
+  oq é language HTML?
+  oq é atributo?
